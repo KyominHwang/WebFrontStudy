9f3cad43d2bf56ebc6114831f4a84e1452bbb3d5
KyominHwang <piter0208@kookmin.ac.kr>
2020-05-02 03:40:32 +0900

    간단한 todolist 만들기

---
+++ todolist/todo.js
@@ -0,0 +1,63 @@
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+let toDos = [];
+
+function loadToDo(){
+    const loadToDos = localStorage.getItem(TODOS_LS);
+    if(loadToDos !== null){
+        const parsedToDos = JSON.parse(loadToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        });
+    }
+}
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function (toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+}
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+function paintToDo(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "X";
+    delBtn.addEventListener("click",deleteToDo);
+    const span = document.createElement("span");
+    span.innerText = text;
+    const newId = toDos.length + 1;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+    li.id = newId;
+    const toDoObj = {
+        text : text,
+        id : newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function init(){
+    loadToDo();
+    toDoForm.addEventListener("submit",handleSubmit);
+}
+
+init();
